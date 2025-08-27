5fbba81643127bb1b8f0552dc5c88f76debf32c7
fatematahir10 <fatemaqureshi9@gmail.com>
2025-08-27 13:00:49 +0500

    Improved hero text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero: React.FC = () => {
       </h1>
 
       <div className="flex gap-4 flex-wrap justify-center mb-10">
-        {["C# Developer", ".NET Expert"].map((item) => (
+        {["C# Developer", ".NET Expert", "Learning React"].map((item) => (
           <div key={item} className="border-2 border-black-400 rounded-md px-4 py-2">
             <p className="text-sm font-semibold">{item}</p>
           </div>
@@ -20,7 +20,9 @@ const Hero: React.FC = () => {
       </div>
 
       <p className="text-lg max-w-6xl leading-relaxed">
-      I’m a results-driven software developer with a strong foundation in .NET C#, specializing in building modern, efficient, and user-friendly desktop and web applications. With over 7 years of development experience, I’ve successfully designed and delivered scalable solutions using clean MVVM and MVC architecture, intuitive UI design, and performance optimization techniques. I thrive in creating practical tools that solve real-world problems. My approach combines clean code, maintainable architecture, and a passion for continuous learning. 
+      I’m a results-driven software developer with a strong foundation in .NET C#, specializing in building modern, efficient, and user-friendly desktop and web applications. With over 7 years of professional experience, I have successfully designed and delivered scalable solutions using clean MVVM and MVC architectures, intuitive UI design, and performance optimization techniques.
+      I thrive on creating practical tools that solve real-world problems, combining clean code, maintainable architecture, and attention to detail. Recently, I’ve been expanding my expertise into modern web development, actively working with React, TypeScript, and Tailwind CSS to build dynamic, responsive applications.
+      Driven by a passion for continuous learning, I embrace new technologies and development practices, ensuring that the solutions I deliver are not only effective today but also adaptable for the future.
       </p>
     </section>
   );
