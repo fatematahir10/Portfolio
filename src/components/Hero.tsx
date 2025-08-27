06e01516488ef30ba23418df40f8301cf9932aa6
fatematahir10 <fatemaqureshi9@gmail.com>
2025-08-27 13:03:06 +0500

    fixed text size

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,7 +19,7 @@ const Hero: React.FC = () => {
         ))}
       </div>
 
-      <p className="text-lg max-w-6xl leading-relaxed">
+      <p className="text-base max-w-6xl leading-relaxed">
       I’m a results-driven software developer with a strong foundation in .NET C#, specializing in building modern, efficient, and user-friendly desktop and web applications. With over 7 years of professional experience, I have successfully designed and delivered scalable solutions using clean MVVM and MVC architectures, intuitive UI design, and performance optimization techniques.
       I thrive on creating practical tools that solve real-world problems, combining clean code, maintainable architecture, and attention to detail. Recently, I’ve been expanding my expertise into modern web development, actively working with React, TypeScript, and Tailwind CSS to build dynamic, responsive applications.
       Driven by a passion for continuous learning, I embrace new technologies and development practices, ensuring that the solutions I deliver are not only effective today but also adaptable for the future.
